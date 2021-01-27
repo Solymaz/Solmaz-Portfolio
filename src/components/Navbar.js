de5c16463e97cd366bec2561124d7ddf0484bd3c
Solymaz <44635076+Solymaz@users.noreply.github.com>
2021-01-27 21:19:54 +0100

    Navbar- Logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./navbar.css";
 
 const Navbar = () => {
-  return <nav className="navbar"></nav>;
+  return <nav className="navbar">
+      <img  src="logo.jpg" className="logo"/>
+      
+  </nav>;
 };
 export default Navbar;
