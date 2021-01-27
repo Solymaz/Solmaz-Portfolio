306c0fc5797b4ac5c032b55fefe28566d9ee53af
Solymaz <44635076+Solymaz@users.noreply.github.com>
2021-01-27 20:04:26 +0100

    Created Navbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
