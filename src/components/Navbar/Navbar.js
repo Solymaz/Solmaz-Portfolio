704ec3fb18b5a3ec63f5509423522383828a7927
Solymaz <44635076+Solymaz@users.noreply.github.com>
2021-02-10 17:17:40 +0100

    nav menu in mobile view

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { NavLink } from "react-router-dom";
 import { BiHomeHeart } from "react-icons/bi";
 import { BsPerson } from "react-icons/bs";
@@ -6,43 +6,76 @@ import { GoSettings } from "react-icons/go";
 import { IoIosCodeWorking } from "react-icons/io";
 import { RiLinkedinLine, RiGithubLine } from "react-icons/ri";
 import { HiOutlineMail } from "react-icons/hi";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 import "./navbar.css";
 
 const Navbar = () => {
+  const menuRef = useRef(null);
+  const [menuShown, setMenuShown] = useState(false);
+
+  const showMenu = () => {
+    if (menuRef.current.style.display === "grid") {
+      menuRef.current.style.display = "none";
+      setMenuShown(false);
+    } else {
+      menuRef.current.style.display = "grid";
+      setMenuShown(true);
+    }
+  };
+
   return (
     <nav className="navbar">
       <NavLink to="/" exact>
         <img src="logo.jpg" className="logo" alt="logo" />
       </NavLink>
-      <div className="information">
-        <NavLink to="/" className="navItem" activeClassName="selected" exact>
-          <BiHomeHeart />
-        </NavLink>
-        <NavLink to="/about" className="navItem" activeClassName="selected">
-          <BsPerson />
-        </NavLink>
-        <NavLink to="/skills" className="navItem" activeClassName="selected">
-          <GoSettings />
-        </NavLink>
-        <NavLink to="/projects" className="navItem" activeClassName="selected">
-          <IoIosCodeWorking />
-        </NavLink>
-        <NavLink to="/contact" className="navItem" activeClassName="selected">
-          <HiOutlineMail />
-        </NavLink>
+      <div className="menu" onClick={showMenu}>
+        {menuShown ? <AiOutlineClose /> : <AiOutlineMenu />}
       </div>
-      <div className="contact">
-        <a
-          href="https://www.linkedin.com/in/solmaz-seyedmousavi/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <RiLinkedinLine className="link" />
-        </a>
-        <a href="https://github.com/Solymaz/" target="_blank" rel="noreferrer">
-          <RiGithubLine className="link" />
-        </a>
+      <div
+        className="menuItems"
+        ref={menuRef}
+        onClick={() => {
+          menuShown && showMenu();
+        }}
+      >
+        <div className="information">
+          <NavLink to="/" className="navItem" activeClassName="selected" exact>
+            <BiHomeHeart />
+          </NavLink>
+          <NavLink to="/about" className="navItem" activeClassName="selected">
+            <BsPerson />
+          </NavLink>
+          <NavLink to="/skills" className="navItem" activeClassName="selected">
+            <GoSettings />
+          </NavLink>
+          <NavLink
+            to="/projects"
+            className="navItem"
+            activeClassName="selected"
+          >
+            <IoIosCodeWorking />
+          </NavLink>
+          <NavLink to="/contact" className="navItem" activeClassName="selected">
+            <HiOutlineMail />
+          </NavLink>
+        </div>
+        <div className="contact">
+          <a
+            href="https://www.linkedin.com/in/solmaz-seyedmousavi/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <RiLinkedinLine className="link" />
+          </a>
+          <a
+            href="https://github.com/Solymaz/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <RiGithubLine className="link" />
+          </a>
+        </div>
       </div>
     </nav>
   );
